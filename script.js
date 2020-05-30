4c6cae0ba1941cc292fcfc64ab164958625b7320
inboxy <james@inboxy.net>
2020-05-30 19:13:06 +0100

    ww

--- script.js
+++ script.js
@@ -1,9 +1,2 @@
-//import { nanoid } from 'nanoid/non-secure'
-
-//const nanoid = customAlphabet('1234567890abcdef', 10)
-
-//var idnumber = nanoid();
 console.log("hello")
-console.log("++++++++++++++++++++++++++++++");
-//console.log("id:" + idnumber);
-console.log("++++++++++++++++++++++++++++++");
+
