a96b8ebba39d450a25f720037030b267170b68a2
inboxy <james@inboxy.net>
2020-05-30 18:48:55 +0100

    js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 import { nanoid } from 'nanoid'
 
+const nanoid = customAlphabet('1234567890abcdef', 10)
+
+var idnumber = nanoid();
+
 console.log("++++++++++++++++++++++++++++++");
-console.log(nanoid());
+console.log("id:" + idnumber;
 console.log("++++++++++++++++++++++++++++++");
