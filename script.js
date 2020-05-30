edd6ff350df80a65962c9379d8e2260a6ec5adb1
inboxy <james@inboxy.net>
2020-05-30 22:09:01 +0100

    ds

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
-console.log("hello");
-console.log(datetime)
 import { nanoid } from 'nanoid'
+
+
+console.log("hello");
+console.log(datetime);
+
 userid = nanoid() //=> "Uakgb_J5m9g-0JDMbcJqLJ"
 
 console.log(userid);
