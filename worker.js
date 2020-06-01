5481cdfe87d7a2c62a52910faa34e20c972fb1e7
inboxy <james@inboxy.net>
2020-06-01 19:50:31 +0100

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,6 +1,6 @@
 console.log("hello from worker");
 
-importScripts(https://cdnjs.cloudflare.com/ajax/libs/nanoid/3.1.9/non-secure/index.min.js);
+importScripts(https://cdnjs.cloudflare.com/ajax/libs/nanoid/3.1.9/non-secure/index.min.js)
 
 import { nanoid } from 'nanoid/non-secure'
 var id = nanoid() //=> "Uakgb_J5m9g-0JDMbcJqLJ"
